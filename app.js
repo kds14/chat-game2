7ad184ab0ce451eeb5629610557f47f61dafb028
Kyle Stratton <kdstratton@gmail.com>
2018-04-18 20:21:53 -0400

    Added socket.io

--- app.js
+++ app.js
@@ -1,43 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+io.on("connection", function(socket) {
+	console.log("CONNECTED");
+
+	socket.on("disconnect", function() {
+		console.log("DISCONNECTED");
+	});
+
+	socket.on("test", function(msg) {
+		console.log("TEST MESSAGE RECEIVED FROM CLIENT");
+		console.log(msg);
+	});
+
+	socket.emit("test-server", "THIS IS A TEST MESSAGE FROM SERVER");
+});
+
+http.listen(3000, () => console.log("Listening on port 3000"))
 
 module.exports = app;
